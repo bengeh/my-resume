6b69e8e145df73efc1f5aa901d3c8a1c2f025f71
1564479 <Benjamin.Geh@sc.com>
2020-12-11 15:28:54 +0800

    changed bar percentage to star rating

--- src/components/SummaryContent.tsx
+++ src/components/SummaryContent.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import styled from 'styled-components'
+import star from '../assets/star.svg'
 
 const summary = [
     {
         desc: "Leadership",
-        currentLevel: 40
+        currentLevel: 4.0
     },
     {
         desc: "Fast Learner",
-        currentLevel: 80
+        currentLevel: 4.0
     },
     {
         desc: "Git, Jenkins, MongoDB",
-        currentLevel: 40
+        currentLevel: 3.0
     },
     {
         desc: "React, Typescript, JavaSCript, Python, Html, CSS",
-        currentLevel: 60
+        currentLevel: 3.5
     },
     {
         desc: "MySQL",
-        currentLevel: 50
+        currentLevel: 2.5
     }
 ]
 
@@ -29,10 +30,27 @@ const SummaryContent = () => {
         <SummaryContainerWrapper>
                 {
                     summary.map((k :any) => {
+                        let starLevel = ""
+                        if(k.currentLevel % 1 === 0){
+                            starLevel = k.currentLevel.toString() + '-0'
+                        }else{
+                            starLevel = k.currentLevel.toString().replace('.', '-')
+                        }
+                        console.log("HEHE", starLevel)
                         return(
                             <SummaryContentContainer key={k.desc}>
                                 <Summary >{k.desc}</Summary>
-                                <SummaryBar currentLevel={k.currentLevel}>{k.currentLevel}%</SummaryBar>
+                                <StyledSVG>
+                                    <use href={`${star}#stars-${starLevel}-star`}></use>
+                                </StyledSVG>
+                                {/* <StyledSVG>
+                                    <use xlinkHref="../assets/star.svg#stars-0-0-star"></use>
+                                </StyledSVG> */}
+                                {/* <svg className="icon">
+                                    <use xlinkHref="../assets/star.svg#stars-0-0-star"></use>
+                                </svg> */}
+                                {/* <StyledSVG src={Star}></StyledSVG> */}
+                                {/* <SummaryBar currentLevel={k.currentLevel}>{k.currentLevel}%</SummaryBar> */}
                             </SummaryContentContainer>
                         )
                     })
@@ -60,5 +78,15 @@ const SummaryBar = styled.div<{currentLevel: number}>`
     text-align: center;
     background: linear-gradient(to right, lightgreen ${(props) => props.currentLevel}%, gray 0);
 `;
+
+const StyledSVG = styled.svg`
+    display: block;
+	height: 18px;
+    width: 102px;
+	
+	&:not(:root) {
+		overflow: hidden;
+	}
+`
 
 export default SummaryContent
